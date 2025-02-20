ce9f55525db1b7e487c625f92b5ece305efdb7bd
Dipesh1009 <94220940+Dipesh1009@users.noreply.github.com>
2025-02-20 09:51:38 +0530

    Square matrix code

--- TestSquareMatrix.java
+++ TestSquareMatrix.java
@@ -13,7 +13,7 @@ class SquareMatrix {
         System.out.println("Enter values of matrix in row major order");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print("Enter Value: ");
+                System.out.print("Enter Value (" + i + "," + j + "): ");
                 matrix[i][j] = sc.nextInt();
             }
         }
@@ -43,12 +43,13 @@ class SquareMatrix {
     }
 
     boolean isSparse() {
-        int limit = (matrix.length * matrix[0].length) * (2/3);
+        int n = matrix[0].length;
+        int limit = (int)((matrix.length * matrix[0].length) * 0.66);
         for (int rows[] : matrix) {
             for (int i : rows) {
                 if (i == 0) {
                     limit--;
-                    if (limit == 0) {
+                    if (limit <= 0) {
                         return true;
                     }
                 }
